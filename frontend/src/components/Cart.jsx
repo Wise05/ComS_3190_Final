b125003395278a17d6574e610e758eabdeea848f
Zephaniah Gustafson <zevangus@gmail.com>
2025-05-06 15:57:06 -0500

    added more shop stuff

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function Cart() {
-  return <div></div>;
+function Cart({ cart, setCart, cartTotal, setStep }) {
+  const itemCounts = cart.reduce((acc, item) => {
+    if (acc[item.id]) {
+      acc[item.id].quantity += 1;
+    } else {
+      acc[item.id] = { ...item, quantity: 1 };
+    }
+    return acc;
+  }, {});
+
+  const groupedItems = Object.values(itemCounts);
+
+  return (
+    <div className="bg-black p-3 min-h-screen z-0 w-120 fixed right-0 top-10 pointer-events-auto">
+      <div className="text-white flex min-width-auto font-bold text-2xl pl-4">
+        <h2>Cart</h2>
+        <button
+          onClick={() => {
+            setStep(null);
+          }}
+          className="ml-auto hover:bg-blue-500 rounded-full px-2"
+        >
+          X
+        </button>
+      </div>
+      <div className="max-h-40 sm:max-h-60 md:max-h-140 text-black overflow-y-auto">
+        {groupedItems.map((item) => {
+          return (
+            <div
+              className="flex gap-5 my-2 px-2 py-3 bg-white rounded-lg h-lg"
+              key={item.id}
+            >
+              <img src={item.image} alt={item.title} className="w-20" />
+              <div>
+                <p>{item.title}</p>
+                <p>x{item.quantity}</p>
+                <p>${item.price}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
--- frontend/src/components/Shop.jsx
+++ frontend/src/components/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Cart from "./Cart";
 
 function Shop() {
   const [products, setProducts] = useState([]);
@@ -88,7 +89,10 @@ function Shop() {
             <div className="flex">
               <button
                 className="bg-blue-500 px-4 py-2 rounded-full font-semi-bold text-white hover:bg-blue-400 mr-auto"
-                onClick={() => addToCart(product)}
+                onClick={() => {
+                  addToCart(product);
+                  console.log(cart);
+                }}
               >
                 add to cart
               </button>
@@ -97,6 +101,43 @@ function Shop() {
           </div>
         ))}
       </div>
+      {step === null ? (
+        <button
+          type="button"
+          onClick={() => {
+            setStep("cart");
+          }}
+          className="fixed top-12 right-3 flex hover:bg-blue-500 hover:text-white pointer-events-auto z-2 px-3 py-1 gap-2 rounded-full"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+            />
+          </svg>
+          <p>View Cart</p>
+        </button>
+      ) : (
+        ""
+      )}
+      {step == "cart" ? (
+        <Cart
+          cart={cart}
+          setCart={setCart}
+          cartTotal={cartTotal}
+          setStep={setStep}
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
