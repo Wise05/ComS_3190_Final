92d845ba110b1c3956a09319b97e2c81b8d3e387
Zephaniah Gustafson <zevangus@gmail.com>
2025-05-08 17:44:20 -0500

    added like button

--- frontend/src/components/Artists.jsx
+++ frontend/src/components/Artists.jsx
@@ -10,6 +10,7 @@ function Artists() {
   const query = searchParams.get("q") || "";
   const [artistData, setArtistData] = useState(null);
   const [videoData, setVideoData] = useState(null);
+  const [likedSongs, setLikedSongs] = useState([]);
 
   useEffect(() => {
     if (query) {
@@ -110,6 +111,14 @@ function Artists() {
     ));
   };
 
+  function isLiked(idTrack) {
+    return likedSongs.some((video) => video.idTrack === idTrack);
+  }
+
+  // useEffect(() => {
+  //   console.log(likedSongs);
+  // }, [likedSongs]);
+
   return (
     <div>
       <div>
@@ -142,22 +151,62 @@ function Artists() {
               {videoData.map((video) => {
                 if (video.strTrackThumb) {
                   return (
-                    <a
-                      href={video.strMusicVid}
-                      target="_blank"
-                      rel="noopener noreferrer"
+                    <div
+                      key={video.idTrack}
+                      className="bg-white rounded-2xl shadow-lg hover:shadow-xl hover:scale-105 transition relative"
                     >
-                      <div className="bg-white rounded-2xl shadow-lg hover:shadow-xl hover:scale-105 transition">
-                        <img
-                          src={video.strTrackThumb}
-                          alt={video.strTrack}
-                          className="w-100 rounded-t-2xl"
-                        />
-                        <h3 className="text-xl font-semibold text-center py-2">
+                      <img
+                        src={video.strTrackThumb}
+                        alt={video.strTrack}
+                        className="w-100 rounded-t-2xl"
+                      />
+                      <a
+                        href={video.strMusicVid}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <h3 className="text-xl font-semibold text-center py-2 hover:font-bold">
                           {video.strTrack}
                         </h3>
-                      </div>
-                    </a>
+                      </a>
+
+                      <button
+                        className="absolute right-1 bottom-1 z-20 pointer-events-auto"
+                        onClick={() => {
+                          setLikedSongs((prev) =>
+                            prev.some((v) => v.idTrack === video.idTrack)
+                              ? prev.filter((v) => v.idTrack !== video.idTrack)
+                              : [...prev, video]
+                          );
+                        }}
+                      >
+                        {isLiked(video.idTrack) ? (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            className="size-6 hover:size-7 text-red-500"
+                          >
+                            <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                          </svg>
+                        ) : (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="size-6 hover:size-7"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                            />
+                          </svg>
+                        )}
+                      </button>
+                    </div>
                   );
                 }
               })}
